e740b175ceaf1648f69119addffa5d1876f68a76
停有钱 <tyq@Development-iMac.local>
2016-09-08 17:21:45 +0800

    s

---
+++ qianghongbaoTests/qianghongbaoTests.m
@@ -0,0 +1,34 @@
+//
+//  fruit2048Tests.m
+//  fruit2048Tests
+//
+//  Created by zhuang chaoxiao on 14-6-2.
+//  Copyright (c) 2014年 zhuang chaoxiao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fruit2048Tests : XCTestCase
+
+@end
+
+@implementation fruit2048Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
